962998200695f01c12da92d7287049686dd36440
SalahAdDin <alagunasalahaddin@live.com>
2017-11-22 14:56:44 +0300

    Lesson 2.5: :sunny: Added `ejs` engine and templates.

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
-server.get('/' (req, res) => {
+server.set('view engine', 'ejs');
 
+server.get('/' (req, res) => {
+  res.render('index');
 });
 
-server.use('/api', apiRouter)
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port: ',config.port );
+});
