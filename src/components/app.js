072fd4dc93cd7b8ead483a7bad193b8c9c43e0dd
SalahAdDin <alagunasalahaddin@live.com>
2017-11-28 00:07:59 +0300

    Lesson 6.7: :sunny: Handling browser's back button and history

--- src/components/app.js
+++ src/components/app.js
@@ -39,6 +39,19 @@ class App extends Component {
     });
   };
 
+  fetchContestList = () => {
+    pushState(
+      { currentContestId: null },
+      '/'
+    );
+    api.fetchContestList().then(contests => {
+      this.setState({
+        currentContestId: null,
+        contests
+      });
+    });
+  };
+
   currentContest(){
     return this.state.contests[this.state.currentContestId];
   }
@@ -53,7 +66,9 @@ class App extends Component {
 
   currentContent(){
     if(this.state.currentContestId){
-      return <Contest {...this.currentContest()} />;
+      return <Contest
+        contestListClick={this.fetchContestList}
+        {...this.currentContest()} />;
     }
     return <ContestList
       onContestClick={this.fetchContest}
