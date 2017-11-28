fa6c3eba2e3186c6c02d6bbb11212ddc9f4fa586
SalahAdDin <alagunasalahaddin@live.com>
2017-11-28 20:14:54 +0300

    Fix: :fire: Replace office fabric styles by boostrap styles.

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Header = ({ message }) => {
   return(
-      <h2 className="ms-textAlignCenter">
+      <h2 className="Header text-center">
         {message}
       </h2>
   );
